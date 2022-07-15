de71bb59dcc5be128fd6bbb93d092bc3bcf93031
Okereke Chinweotito <okerekechinweotito@gmail.com>
2022-07-15 15:19:19 +0100

    clear up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,31 +2,7 @@ import React from "react";
 import { Grid } from "@mui/material";
 
 const Home = () => {
-  return (
-    <Grid
-      container
-      columns={{ xs: 4, sm: 8, md: 12 }}
-      component="article"
-      sx={{ justifyContent: "center" }}
-    >
-      <Grid
-        container
-        item
-        xs={4}
-        sm={8}
-        md={10}
-        sx={{
-          height: { xs: "25em", md: "40em" },
-          flexDirection: { xs: "column", md: "row" },
-          justifyContent: "space-between",
-          background: "red",
-        }}
-      >
-        <section>Side 1</section>
-        <section>Side 2</section>
-      </Grid>
-    </Grid>
-  );
+  return <h1>components</h1>;
 };
 
 export default Home;
